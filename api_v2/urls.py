5ecca0a85712c3c6d296b50f5d8a8ac09560cfcb
Adam <adam.nemmish213@gmail.com>
2024-02-06 10:23:06 +0600

    Реализовал детальный просмотр-Detailview, так же реализовал обновление-Updateview

--- api_v2/urls.py
+++ api_v2/urls.py
@@ -6,5 +6,6 @@ app_name = 'api_v2'
 urlpatterns = [
     path('echo/', json_echo_view, name='echo'),
     path('token/', get_token_view, name='get_token'),
-    path('article/', ArticleView.as_view(), name='article')
+    path('article/', ArticleView.as_view(), name='article'),
+    path('article/<int:pk>/', ArticleView.as_view())
 ]
--- api_v2/views.py
+++ api_v2/views.py
@@ -1,12 +1,12 @@
 import json
 
+from django.shortcuts import get_object_or_404
 from django.views import View
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from webapp.models import Article
 from api_v2.serializers import ArticleSerializer, ArticleModelSerializer
 
@@ -31,21 +31,34 @@ def json_echo_view(request, *args, **kwargs):
 class ArticleView(APIView):
 
     def get(self, request, *args, **kwargs):
-        # article = Article.objects.first()
-        # serializer = ArticleSerializer(article)
-        # print(serializer.data)
-        # return JsonResponse(serializer.data)
+        pk = self.kwargs.get('pk')
+        if pk:
+            article = get_object_or_404(Article, pk=pk)
+            serializer = ArticleModelSerializer(article)
+            return Response(serializer.data)
         articles = Article.objects.order_by('-created_at')
         articles_list = ArticleModelSerializer(articles, many=True).data
         return Response(articles_list)
 
     def post(self, request, *args, **kwargs):
         serializer = ArticleModelSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     article = serializer.save()
-        #     return JsonResponse(serializer.data, safe=False)
-        # return JsonResponse({'error': serializer.errors}, status=400)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
+    def put(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        article = get_object_or_404(Article, pk=pk)
+        if pk:
+            serializer = ArticleModelSerializer(article, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            serializer = ArticleModelSerializer(article, data=request.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        article = get_object_or_404(Article, pk=pk)
+        article.delete()
